460bd064b137ac1e1de47fc656dc905c14d858d1
Jan Šleichert <jan.sleichert@gmail.com>
2023-12-07 09:07:18 +0100

    Splnění domácího úkolu

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+
 console.log('funguju!');
+const alarm = document.querySelector(".alarm")
+const btn = document.querySelector("#btn")
+
+
+const ring = () =>{
+    alarm.classList.add("alarm--ring")
+    document.getElementById("audio").play()
+}
+const question = prompt("Za kolik vteřin má budík zazvonit?")
+const timeoutId = setTimeout(ring, question*1000);
+
+
+btn.addEventListener("click",() => {clearTimeout(timeoutId)})
+
+
